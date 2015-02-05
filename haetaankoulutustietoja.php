5b69fda13d1a02b132ce4ee05e5834f3db39abb8
famas-- <anttila.tuomas@hotmail.com>
2015-02-05 19:42:52 +0200

    kommentointeja parannettu

--- haetaankoulutustietoja.php
+++ haetaankoulutustietoja.php
@@ -3,15 +3,18 @@
 //sisällytetään yhteystidot
 include 'yhteys.php';
 
+
+//määritellään muuttuja
 $q = $_GET['q'];
 
 
+//haetaan koulutuksen id:llä koulutuksen paikka, toteuttaja ja ajankonta. echotaan tulokset. ajan muoto muutetaan suomalaiseksi 
 $sql = mysqli_query($yhteys,"SELECT * FROM koulutus WHERE koulutusid ='".$q."'");
 while($tulos = mysqli_fetch_array($sql)){
   echo "<table><tr>";
-  echo "<td width=\"250px\">Paikka</td><td>" . $tulos['paikka'] . "</td><tr>";
-  echo "<td width=\"250px\">Toteuttaja</td><td>" . $tulos['toteuttaja'] . "</td><tr>";
-  echo "<td width=\"250px\">Ajankohta</td><td>" . date("d.m.Y", strtotime($tulos['aloitusaika'])) . " - " . date("d.m.Y", strtotime($tulos['lopetusaika'])) . "</td><tr>";
+  echo "<td width=\"150px\">Paikka:</td><td>" . $tulos['paikka'] . "</td><tr>";
+  echo "<td width=\"150px\">Toteuttaja:</td><td>" . $tulos['toteuttaja'] . "</td><tr>";
+  echo "<td width=\"150px\">Ajankohta:</td><td>" . date("d.m.Y", strtotime($tulos['aloitusaika'])) . " - " . date("d.m.Y", strtotime($tulos['lopetusaika'])) . "</td><tr>";
   echo "</tr></table>";
 }
 
