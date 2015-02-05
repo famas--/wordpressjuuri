e6ce96951aeb098e10cb4b945b0675ba4519fc30
famas-- <anttila.tuomas@hotmail.com>
2015-02-05 19:46:05 +0200

    kommentointia parannettu

--- tavarailmoittautuneettaulukkoon.php
+++ tavarailmoittautuneettaulukkoon.php
@@ -25,7 +25,7 @@ $ammatti = $_POST['ammatti'];
 
 
 
-// viedään $nimi variableen tallennettu tieto koulutustaulukkoon tietueeseen koulutuksen nimi
+// viedään muuttuja $nimi tallennettu tieto ilmoittautuneet taulukkoon
 $query = "INSERT INTO ilmoittautuneet (
 		koulutusid,
 		yritys,
@@ -44,7 +44,7 @@ if (!mysqli_query($yhteys,$query)) {
     die("error: " . mysqli_error($yhteys));
 } 
 echo "<table class=\"tuomastablecenter\"><td><h1 >Ilmoittautumisesi onnistui!</h1></td><tr>
-	<td><form action=\"http://localhost/wordpress/\"><input type=\"submit\" value=\"Takaisin etusivulle\"></form></td></table>";
+	<td><form action=\"http://www.nihakseutu.com/wordpress/\"><input type=\"submit\" value=\"Takaisin etusivulle\"></form></td></table>";
 
 
 // suljetaan yhteys
